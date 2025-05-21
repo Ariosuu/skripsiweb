65422795898b032b32cd67b3a49f782a45ed2bad
Ariosuu <priambodoaryo4@gmail.com>
2025-05-21 15:53:58 +0700

    icon fonts
    added mdi/js, tweaked vuetify.js plugin

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,20 @@
-// Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
-// Vuetify
-import { createVuetify } from 'vuetify'
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+const vuetify = createVuetify({
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+  components,
+  directives,
+});
+
+export default vuetify;

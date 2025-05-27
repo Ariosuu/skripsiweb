6084bf024c8e6e19fad07fcce780151e497cc9dc
LudensNoir <nichoarthur17@gmail.com>
2025-05-27 18:49:59 +0700

    Add Table and History

--- src/views/TimeOffView.vue
+++ src/views/TimeOffView.vue
@@ -21,9 +21,40 @@
       </v-card>
 
       <v-row class="pt-4">
-        <v-col cols="8"></v-col>
+        <v-col cols="8">
+          <span class="pl-2 text-h6 font-weight-bold">Leave Request</span>
+          <v-data-table
+            :headers="headers"
+            :items="timeRequest"
+            hide-default-footer
+          >
+            <template v-slot:item.detail>
+              <v-btn icon flat size="sm">
+                <v-icon :icon="mdiEye" color="#1985A1" />
+              </v-btn>
+            </template>
+          </v-data-table>
+        </v-col>
         <v-col cols="4">
-          <v-card flat color="#D9D9D9"><v-card-text>Test</v-card-text></v-card>
+          <v-card flat color="#D9D9D9">
+            <v-card-title class="text-h6">Leave Request History</v-card-title>
+            <v-card-text>
+              <v-card flat>
+                <v-card-text class="pa-0 bg-transparent|">
+                  <v-card
+                    title="Sick Leave"
+                    subtitle="18/06/2048 - 26/06/2048"
+                    flat
+                    class="pa-0"
+                  >
+                    <template v-slot:append>
+                      <v-chip color="green">Approved</v-chip>
+                    </template>
+                  </v-card>
+                </v-card-text>
+              </v-card>
+            </v-card-text>
+          </v-card>
         </v-col>
       </v-row>
     </v-card-text>
@@ -31,5 +62,23 @@
 </template>
 
 <script setup>
-import { mdiLogout } from "@mdi/js";
+import { mdiEye, mdiLogout } from "@mdi/js";
+import { ref } from "vue";
+
+const timeRequest = ref([
+  {
+    type: "Diddy Do It",
+    from: "18/02/2025",
+    to: "18/02/2025",
+    notes: "Baby Powder",
+  },
+]);
+
+const headers = ref([
+  { title: "Type", key: "type", align: "start" },
+  { title: "From", key: "from", align: "center" },
+  { title: "To", key: "to", align: "center" },
+  { title: "Notes", key: "notes", align: "start" },
+  { title: "Detail", key: "detail", align: "center", sortable: false },
+]);
 </script>

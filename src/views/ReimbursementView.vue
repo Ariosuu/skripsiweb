eb77674b190215f4facb022f4cd142f8e8ab0a94
LudensNoir <nichoarthur17@gmail.com>
2025-06-09 16:09:18 +0700

    Add new an view dialog to Reimbursement

--- src/views/ReimbursementView.vue
+++ src/views/ReimbursementView.vue
@@ -25,7 +25,9 @@
     </v-col>
     <v-spacer />
     <v-col class="d-flex align-center justify-end pr-7">
-      <v-btn flat color="#1985A1">Add New Reimburse</v-btn></v-col
+      <v-btn flat color="#1985A1" @click="newReimburse"
+        >Add New Reimburse</v-btn
+      ></v-col
     >
   </v-row>
 
@@ -64,7 +66,12 @@
       </template>
 
       <template v-slot:item.detail="{ item }">
-        <v-btn icon flat size="sm" @click="console.log('test')">
+        <v-btn
+          icon
+          flat
+          size="sm"
+          @click="viewReimburse(reimbursement.indexOf(item))"
+        >
           <v-icon :icon="mdiEye" color="#1985A1" />
         </v-btn>
       </template>
@@ -75,15 +82,98 @@
     </v-data-table>
   </v-card>
 
-  <v-dialog v-model="reimburseDialog"> </v-dialog>
+  <v-dialog v-model="reimburseDialog" width="auto" persistent>
+    <v-form @submit.prevent="test" v-model="isValid">
+      <v-card
+        width="1200"
+        :title="isNew ? 'Add New Claim' : reimburseForm.claimId"
+      >
+        <template v-slot:append>
+          <v-btn :icon="mdiClose" flat size="sm" @click="closeDialog"></v-btn>
+        </template>
+
+        <v-card-text class="pa-4 pt-2">
+          <v-row>
+            <v-col cols="8">
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field
+                    label="Customer Name"
+                    v-model="reimburseForm.name"
+                    disabled
+                    variant="outlined"
+                    density="compact"
+                    width="auto"
+                    hide-details
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-date-input
+                    v-model="reimburseForm.date"
+                    label="Date"
+                    variant="outlined"
+                    density="compact"
+                    prepend-icon=""
+                    :prepend-inner-icon="mdiCalendar"
+                    hide-details
+                    :readonly="!isNew"
+                  ></v-date-input>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    label="Amount"
+                    v-model="reimburseForm.bill"
+                    variant="outlined"
+                    density="compact"
+                    width="auto"
+                    type="number"
+                    hide-details
+                    :readonly="!isNew"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-textarea
+                    label="Notes"
+                    variant="outlined"
+                    v-model="reimburseForm.notes"
+                    hide-details
+                    :readonly="!isNew"
+                  ></v-textarea>
+                </v-col>
+              </v-row>
+            </v-col>
+
+            <v-col cols="4">
+              <v-card color="#D9D9D9" flat class="fill-height">
+                <v-card-text>PLACE HOLDER</v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="3" v-if="!isNew">
+              Claim Status :
+              <v-chip :color="chipColor(reimburseForm.status)">
+                {{ reimburseForm.status }}
+              </v-chip>
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions v-if="isNew">
+          <v-btn type="submit" text="Submit" color="#1985A1" variant="flat" />
+        </v-card-actions>
+      </v-card>
+    </v-form>
+  </v-dialog>
 </template>
 
 <script setup>
-import { mdiFilter, mdiLogout, mdiEye } from "@mdi/js";
+import { mdiFilter, mdiLogout, mdiEye, mdiClose, mdiCalendar } from "@mdi/js";
 import { ref } from "vue";
+import { VDateInput } from "vuetify/labs/VDateInput";
 
 const currentMonth = ref("February");
 const reimburseDialog = ref(false);
+const isValid = ref(false);
+const isNew = ref(false);
 
 const headers = ref([
   { title: "Claim Number", key: "claimId", align: "start", width: 175 },
@@ -112,6 +202,47 @@ const reimbursement = ref([
     bill: 20000,
     approved: 10000,
     status: "Approved",
+    notes: "Eric stole my food",
   },
 ]);
+
+const reimburseForm = ref({
+  name: "Daniel Garyo",
+  claimId: "",
+  date: new Date(),
+  bill: 0,
+  approved: 0,
+  status: "Pending",
+  notes: "",
+});
+
+const newReimburse = () => {
+  isNew.value = true;
+  reimburseDialog.value = true;
+};
+
+const viewReimburse = (x) => {
+  isNew.value = false;
+  reimburseForm.value = reimbursement.value[x];
+  reimburseDialog.value = true;
+};
+
+const closeDialog = () => {
+  reimburseForm.value = {
+    name: "Daniel Garyo",
+    claimId: "",
+    date: new Date(),
+    bill: 0,
+    approved: 0,
+    status: "Pending",
+    notes: "",
+  };
+  reimburseDialog.value = false;
+};
+
+const test = () => {
+  if (isValid.value) {
+    closeDialog();
+  }
+};
 </script>

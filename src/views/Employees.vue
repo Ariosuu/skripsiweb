1c57c49cb0cf7fcc9a24a7a1b6dc6ea87a225088
LudensNoir <nichoarthur17@gmail.com>
2025-06-13 17:16:24 +0700

    Adjust UI dan Layout

--- src/views/Employees.vue
+++ src/views/Employees.vue
@@ -1,67 +1,56 @@
 <template>
   <v-app-bar color="#C5C3C6" flat height="64">
-    <p color="#C5C3C6" class="text-h6 font-weight-bold pl-8">Employees</p>
+    <p color="#C5C3C6" class="text-h6 font-weight-bold pl-8">Employee List</p>
     <v-spacer></v-spacer>
     <v-avatar color="surface-variant" size="small"> </v-avatar>
     <v-btn :icon="mdiLogout"> </v-btn>
   </v-app-bar>
+  <v-row>
+    <v-col cols="3">
+      <v-card-text>
+        <v-text-field
+          :loading="loading"
+          :append-inner-icon="mdiMagnify"
+          density="compact"
+          label="Search Employees"
+          variant="solo"
+          hide-details
+          single-line
+          flat
+          @click:append-inner="onClick"
+        >
+        </v-text-field>
+      </v-card-text>
+    </v-col>
+  </v-row>
 
-  <v-col cols="12" class="pl-10 pt-10">
-    <v-row align="center">
-      <v-col cols="9">
-        <h1 class="text-grey-darken-3">Employee List</h1>
-      </v-col>
-      <v-col cols="3">
-        <v-card-text>
-          <v-text-field
-            :loading="loading"
-            :append-inner-icon="mdiMagnify"
-            density="compact"
-            label="Search Employees"
-            variant="solo"
-            hide-details
-            single-line
-            @click:append-inner="onClick"
-          >
-          </v-text-field>
-        </v-card-text>
-      </v-col>
-    </v-row>
-  </v-col>
+  <v-card max-height="750" class="ma-4 mt-0" color="#FFFFFF">
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      class="px-4"
+      height="750"
+      hide-default-footer
+      items-per-page="-1"
+    >
+      <template v-slot:item.dob="{ value }">
+        {{ value.toLocaleDateString() }}
+      </template>
 
-  <v-card height="710" class="pa-4 ml-5 mr-5" color="#FFFFFF">
-    <v-table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Division</th>
-          <th>Position</th>
-          <th>Date of Birth</th>
-          <th>Contact Number</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(item, index) in items" :key="index">
-          <td>{{ item.name }}</td>
-          <td>{{ item.div }}</td>
-          <td>{{ item.pos }}</td>
-          <td>{{ item.dob }}</td>
-          <td>{{ item.cont }}</td>
-          <td>
-            {{ item.stat }}
-            <v-btn rounded="xl" variant="text" :ripple="false">
-              <v-icon
-                color="#1985A1"
-                slot="prepend-icon"
-                :icon="mdiEyeOutline"
-                :size="20"
-              />
+      <template v-slot:item.status="{ value }">
+        <v-chip :color="chipColor(value)">{{ value }}</v-chip>
+      </template>
+
+      <template v-slot:item.view="{ item }">
+        <v-tooltip text="View Employee Profile">
+          <template v-slot:activator="{ props }">
+            <v-btn icon flat v-bind="props" size="sm">
+              <v-icon :icon="mdiEye" color="#1985A1" />
             </v-btn>
-          </td>
-        </tr>
-      </tbody>
-    </v-table>
+          </template>
+        </v-tooltip>
+      </template>
+    </v-data-table>
   </v-card>
 </template>
 
@@ -73,31 +62,50 @@ import {
   mdiLogoutVariant,
   mdiMagnify,
   mdiEyeOutline,
+  mdiEye,
 } from "@mdi/js";
 
 import { ref } from "vue";
+
+const headers = ref([
+  { title: "Name", key: "name", align: "start", width: 320 },
+  { title: "Division", key: "div", align: "start" },
+  { title: "Position", key: "pos", align: "start" },
+  { title: "Date Of Birth", key: "dob", align: "center" },
+  { title: "Contact Number", key: "contact", align: "start", width: 200 },
+  { title: "Status", key: "status", align: "center", sortable: false },
+  { title: "", key: "view", align: "center", sortable: false },
+]);
 
 const items = ref([
   {
     name: "Eric Sebastian Chandra",
     div: "Human Resource",
     pos: "Team Leader",
-    dob: "06/12/2002",
-    cont: "08118300806",
-    stat: "Active",
+    dob: new Date("06/12/2002"),
+    contact: "08118300806",
+    status: "Active",
   },
   {
     name: "Daniel Garyo",
     div: "IT Development",
     pos: "UX Designer",
-    dob: "06/12/2002",
-    cont: "08118300806",
-    stat: "Active",
+    dob: new Date("06/12/2002"),
+    contact: "08118300806",
+    status: "Active",
   },
 ]);
 
 const loaded = ref(false);
 const loading = ref(false);
+
+const chipColor = (x) => {
+  if (x == "Active") {
+    return "green";
+  } else if (x == "Inactive") {
+    return "red";
+  }
+};
 
 function onClick() {
   loading.value = true;

1815cd327cbc35bc963f50eb20066b48750ecc16
LudensNoir <nichoarthur17@gmail.com>
2025-06-16 12:34:13 +0700

    Add New Employee Dialog

--- src/views/Employees.vue
+++ src/views/Employees.vue
@@ -23,6 +23,13 @@
         </v-text-field>
       </v-card-text>
     </v-col>
+
+    <v-spacer />
+    <v-col class="d-flex align-center justify-end pr-7">
+      <v-btn flat color="#1985A1" @click="newEmployee"
+        >Add New Employee</v-btn
+      ></v-col
+    >
   </v-row>
 
   <v-card max-height="750" class="ma-4 mt-0" color="#FFFFFF">
@@ -72,6 +79,109 @@
       </template>
     </v-data-table>
   </v-card>
+
+  <v-dialog v-model="employeeDialog" width="auto" persistent>
+    <v-form @submit.prevent="confirmEmployee" v-model="isValid">
+      <v-card width="800" title="New Employee">
+        <template v-slot:append>
+          <v-btn :icon="mdiClose" flat size="sm" @click="closeDialog"></v-btn>
+        </template>
+
+        <v-card-text class="pa-4 pt-2">
+          <v-row>
+            <v-col cols="6" class="pb-0">
+              <v-text-field
+                label="First Name"
+                v-model="employeeForm.firstName"
+                variant="outlined"
+                density="compact"
+                width="auto"
+                :rules="[rules.required('Cannot be Empty')]"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6" class="pb-0">
+              <v-text-field
+                label="Last Name"
+                v-model="employeeForm.lastName"
+                variant="outlined"
+                density="compact"
+                width="auto"
+                :rules="[rules.required('Cannot be Empty')]"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="8" class="pt-1 pb-0">
+              <v-date-input
+                label="Date of Birth"
+                v-model="employeeForm.dateOfBirth"
+                variant="outlined"
+                density="compact"
+                width="auto"
+                prepend-icon=""
+                :append-inner-icon="mdiCalendar"
+                :rules="[rules.required('Cannot be Empty')]"
+              ></v-date-input>
+            </v-col>
+            <v-col cols="4" class="pt-1 pb-0">
+              <v-text-field
+                label="Phone Number"
+                v-model="employeeForm.phone"
+                variant="outlined"
+                density="compact"
+                width="auto"
+                :append-inner-icon="mdiPhone"
+                :rules="[rules.required('Cannot be Empty')]"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" class="pt-1 pb-0">
+              <v-text-field
+                label="Email"
+                v-model="employeeForm.email"
+                variant="outlined"
+                density="compact"
+                width="auto"
+                :append-inner-icon="mdiAt"
+                :rules="[rules.required('Cannot be Empty')]"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="4" class="pt-1 pb-0">
+              <v-text-field
+                label="Job Division"
+                v-model="employeeForm.division"
+                variant="outlined"
+                density="compact"
+                width="auto"
+                :rules="[rules.required('Cannot be Empty')]"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="4" class="pt-1 pb-0">
+              <v-text-field
+                label="Job Title"
+                v-model="employeeForm.jobTitle"
+                variant="outlined"
+                density="compact"
+                width="auto"
+                :rules="[rules.required('Cannot be Empty')]"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="4" class="pt-1 pb-0">
+              <v-select
+                label="Status"
+                v-model="employeeForm.status"
+                :items="['Active', 'Inactive']"
+                variant="outlined"
+                density="compact"
+                width="auto"
+                :rules="[rules.required('Cannot be Empty')]"
+              ></v-select>
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn type="submit" text="Save" color="#1985A1" variant="flat" />
+        </v-card-actions>
+      </v-card>
+    </v-form>
+  </v-dialog>
 </template>
 
 <script setup>
@@ -83,22 +193,41 @@ import {
   mdiMagnify,
   mdiEyeOutline,
   mdiEye,
+  mdiClose,
 } from "@mdi/js";
 
-import { ref, watch, computed } from "vue";
+import { ref, watch, computed, reactive } from "vue";
 import { db } from "@/firebase/config";
 import { Timestamp } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { signOut } from "firebase/auth";
 import { projectAuth } from "@/firebase/config";
 import getCollection from "@/composables/getCollection";
+import { VDateInput } from "vuetify/labs/VDateInput";
+import { useRules } from "vuetify/labs/rules";
 
 const loaded = ref(false);
 const loading = ref(false);
 const search = ref("");
 
+const employeeDialog = ref(false);
+
+const isValid = ref(false);
+const rules = useRules();
+
 const auth = getAuth();
 const { documents: items } = getCollection("employees");
+
+const employeeForm = reactive({
+  firstName: "",
+  lastName: "",
+  dateOfBirth: new Date(),
+  phone: "",
+  email: "",
+  jobTitle: "",
+  division: "",
+  status: "Active",
+});
 
 watch(
   items,
@@ -158,6 +287,20 @@ const headers = ref([
   { title: "Status", key: "empStatus", align: "center", sortable: false },
   { title: "", key: "view", align: "center", sortable: false },
 ]);
+
+const newEmployee = () => {
+  employeeDialog.value = true;
+};
+
+const closeDialog = () => {
+  employeeDialog.value = false;
+};
+
+const confirmEmployee = () => {
+  if (isValid.value) {
+    closeDialog();
+  }
+};
 
 const chipColor = (x) => {
   if (x == "Active") {
